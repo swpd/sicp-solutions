cbee295d1d49c6165bf195e6db8c9f384310c375
swpd <swpdtz@gmail.com>
2014-09-22 11:26:49 +0800

    add 1.25.scm

---
+++ chap1/1.25.scm
@@ -0,0 +1,11 @@
+; Alyssa's solution is correct but is much slower than the origin one.
+; The modified version of procedure `expmod' will generate huge intermediate
+; results which will make it considerably longer to get the same final result.
+
+(define (expmod base exp m)
+  (remainder (fast-expt base exp) m))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
