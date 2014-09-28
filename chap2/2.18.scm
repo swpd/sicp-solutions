dde029a2babd13487b51598b2d303b8e8bf5c996
swpd <swpdtz@gmail.com>
2014-09-28 16:40:37 +0800

    fix list append

--- chap2/2.18.scm
+++ chap2/2.18.scm
@@ -1,6 +1,6 @@
 (define (reverse items)
   (if (null? items)
     items
-    (cons (reverse (cdr items)) (car items))))
+    (append (reverse (cdr items)) (list (car items)))))
 
 (reverse (list 1 4 9 16 25))    ;Value: (25 16 9 4 1)
