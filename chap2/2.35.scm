196314ed47c38be3c32d4ce0943c31447fc92472
swpd <swpdtz@gmail.com>
2014-10-01 00:54:20 +0800

    add 2.35.scm

---
+++ chap2/2.35.scm
@@ -0,0 +1,16 @@
+(define nil (list))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (x) 1) (enumerate-tree t))))
