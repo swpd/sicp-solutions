638948fbeefed49359dadbdc22fb8a750673a3e6
swpd <swpdtz@gmail.com>
2014-10-07 00:19:57 +0800

    add 2.48.scm

---
+++ chap2/2.48.scm
@@ -0,0 +1,11 @@
+(define (make-vect x y) (cons x y))
+
+(define (xcor-vect v) (car v))
+
+(define (ycor-vect v) (cdr v))
+
+(define (make-segment start-vec end-vec) (cons start-vec end-vec))
+
+(define (start-segment segment) (car segment))
+
+(define (end-segment segment) (cdr segment))
